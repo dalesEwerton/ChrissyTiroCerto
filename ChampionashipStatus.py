a92a447e2743d25b27938f9579092347fe00ccaa
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2019-01-01 17:58:51 -0200

    Write results on CSV

--- ChampionashipStatus.py
+++ ChampionashipStatus.py
@@ -3,3 +3,9 @@ def getCalcioSerieA():
     calcioSerieA.append(('HomeMakeMean', 1.47))
     calcioSerieA.append(('VisitorMakeMean', 1.14))
     return calcioSerieA
+
+def getPremierLeague():
+    premier = []
+    premier.append(('HomeMakeMean', 1.53))
+    premier.append(('VisitorMakeMean', 1.31))
+    return premier
--- Chrissy.py
+++ Chrissy.py
@@ -1,9 +1,9 @@
 import ChampionashipStatus as CS
 import ConstantGenerator as CG
 
-def predictCalcioSerieA(makeHomeMean, takeHomeMean, makeVisitorMean, takeVisitorMean):
-    homeMeanC = CS.getCalcioSerieA()[0][1]
-    visitorMeanC = CS.getCalcioSerieA()[1][1]
+def predictCalcioSerieA(teamHome, teamVisitor, makeHomeMean, takeHomeMean, makeVisitorMean, takeVisitorMean):
+    homeMeanC = CS.getPremierLeague()[0][1]
+    visitorMeanC = CS.getPremierLeague()[1][1]
     ofensive = CG.ofensiveCap(makeHomeMean, makeVisitorMean, homeMeanC, visitorMeanC)
     defensive = CG.defensiveCap(takeHomeMean, takeVisitorMean, visitorMeanC, homeMeanC)
     homeGoals = CG.predictHomeGoalsLambda(ofensive[0][1], defensive[1][1], homeMeanC)
@@ -14,34 +14,16 @@ def predictCalcioSerieA(makeHomeMean, takeHomeMean, makeVisitorMean, takeVisitor
     draw = CG.generateDrawnProb(homeGoals, visitorGoals)
     homeV = CG.generateVictoryProb(homeGoals, visitorGoals)
     homeL = CG.generateLooseProb(homeGoals, visitorGoals)
+    file_obj = open("data/results.csv", "a")
+    new_line = ("\n" + teamHome + "," + teamVisitor + "," + "Premier League," + str(homeV) + "," + str(draw) + "," + str(homeL) + "," + str(chrissyConstant)+",")
+    print(file_obj)
+    file_obj.write(new_line)
+    file_obj.close()
+    print(teamHome + " x " + teamVisitor)
     print("Vitoria da Casa - "  + str(homeV*100) + " %")
     print("Empate - " + str(draw*100) + " %")
     print("Vitoria Visitante - " + str(homeL*100) + " %")
+    print("Jogos - 10")
     print("Chrissy Constant - " + str(chrissyConstant))
-
-
-print("Juventus - Sampdoria")
-predictCalcioSerieA(7/4,1/4,8/4,9/4)
-
-print("Chievo - Frosinone Calcio")
-predictCalcioSerieA(4/4,6/4,1/4,8/4)
-
-print("Empoli - Inter Milan")
-predictCalcioSerieA(9/4,8/4,4/4,8/4)
-
-print("Genoa - Fiorentina")
-predictCalcioSerieA(6/4,5/4,5/4,4/4)
-
-print("Lazio - Torino")
-predictCalcioSerieA(10/4,5/4,5/4,2/4)   
-
-print("Parma - Roma")
-predictCalcioSerieA(3/4,2/4,3/4,5/4)
-
-print("Sassuolo - Atalanta")
-predictCalcioSerieA(5/4,5/4,8/4,8/4)
-
-print("Udinese - Cagliari")
-predictCalcioSerieA(3/4,5/4,5/4,9/4)
 
 
